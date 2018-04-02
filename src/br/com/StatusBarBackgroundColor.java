b9a03214e6c2639356dd8a86a72685720725232f
JorgeDanilo <sobrejavatec@gmail.com>
2018-04-02 15:49:49 -0300

    melhorias

--- src/br/com/StatusBarBackgroundColor.java
+++ src/br/com/StatusBarBackgroundColor.java
@@ -1,9 +1,8 @@
 package br.com;
 
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
-import org.json.JSONArray;
-import org.json.JSONException;
+import org.apache.cordova.CordovaWebView;
 
 import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
@@ -14,36 +13,12 @@ import android.view.WindowManager.LayoutParams;
 public class StatusBarBackgroundColor extends CordovaPlugin {
 
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callback) throws JSONException {
-        // grab the correct methods
-        if(action.equalsIgnoreCase("enable")) {
-            if(VERSION.SDK_INT >= VERSION_CODES.KITKAT) {
-                cordova.getActivity().runOnUiThread( new Runnable() {
-                    public void run() {
-                        cordova.getActivity().getWindow().addFlags(LayoutParams.FLAG_TRANSLUCENT_STATUS);
-                    }
-                });
-                callback.success();
-            } else {
-                callback.error("not supported");
-            }
-            return true;
-        } else if(action.equalsIgnoreCase("disable")) {
-            if(VERSION.SDK_INT >= VERSION_CODES.KITKAT) {
-                cordova.getActivity().runOnUiThread( new Runnable() {
-                    public void run() {
-                        cordova.getActivity().getWindow().clearFlags(LayoutParams.FLAG_TRANSLUCENT_STATUS);
-                    }
-                });
-                callback.success();
-            } else {
-                callback.error("not supported");
-            }
-            return true;
-        } else {
-            callback.error("Unknown Action: " + action);
-            return false;
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        if(VERSION.SDK_INT >= VERSION_CODES.KITKAT) {
+            cordova.getActivity().getWindow().addFlags(LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
+
     }
 
 }
